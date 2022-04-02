c98b0c8c8155b505dbb57775f691da5fba7ca90e
Jamshid1986 <65701335+Jamshid1986@users.noreply.github.com>
2022-04-02 20:46:39 +0500

    context va userlar bilan ishlash

--- customersapp/views.py
+++ customersapp/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from . import models
 # Create your views here.
+
+def home(request):
+    customers = models.Customer.objects.all()
+
+    context = {
+        "customers": customers
+    }
+    return render(request, 'index.html', context)
+
